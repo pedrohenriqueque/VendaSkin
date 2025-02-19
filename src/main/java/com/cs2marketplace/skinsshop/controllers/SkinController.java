feaba35eb63cae9c9e6a226792b9fdf19060ea65
pedrohenriqueque <108901318+pedrohenriqueque@users.noreply.github.com>
2025-02-19 16:20:02 -0300

    Utilizando a API da steam para puxar as skins e armazenalas no DB

--- src/main/java/com/cs2marketplace/skinsshop/controllers/SkinController.java
+++ src/main/java/com/cs2marketplace/skinsshop/controllers/SkinController.java
@@ -1,11 +1,15 @@
 package com.cs2marketplace.skinsshop.controllers;
 
 import com.cs2marketplace.skinsshop.model.Skin;
+import com.cs2marketplace.skinsshop.repository.SkinRepository;
 import com.cs2marketplace.skinsshop.services.SkinService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -14,9 +18,11 @@ import java.util.Map;
 
 public class SkinController {
     private final SkinService skinService;
+    private final SkinRepository skinRepository;
 
-    public SkinController(SkinService skinService) {
+    public SkinController(SkinService skinService, SkinRepository skinRepository) {
         this.skinService = skinService;
+        this.skinRepository= skinRepository;
     }
 
     @GetMapping
@@ -27,7 +33,32 @@ public class SkinController {
     public Skin getSkinById(@PathVariable long id) {
         return  skinService.getSkinById(id);
     }
+    @GetMapping("/fetch")
+    public ResponseEntity<Map<String, String>> fetchSkins() {
+        String url = "https://api.steamapis.com/image/items/730"; // URL da API
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
 
+        if (response.getStatusCode().is2xxSuccessful()) {
+            Map<String, String> skins = response.getBody();
+
+            // Salvar os skins no banco de dados
+            for (Map.Entry<String, String> entry : skins.entrySet()) {
+                Skin skin = new Skin();
+                skin.setName(entry.getKey());
+                skin.setImageUrl(entry.getValue());
+                skin.setPrice(BigDecimal.valueOf(Math.random()));
+                skin.setRarity("vermelha");
+                skin.setTipo("teste");
+                skinRepository.save(skin);
+
+            }
+
+            return ResponseEntity.ok(skins);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
     @PostMapping
     public Skin addSkin(@RequestBody Skin skin) {
         return skinService.addSkin(skin);
